54073bd51787e5e7fc4ff71c9e52a0fe98b4d6a4
Candice Peters <candice.peters@flatironschool.com>
2021-05-22 19:35:43 -0500

    commit tree 4dc2ce739bbf84a34f8df888a7b02c8207967a04

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do 
+        erb :index
+    end
+
+    post '/checkout' do 
+        # binding.pry
+        @item = params["item"]
+        session[:item] = @item
+    end
 end
